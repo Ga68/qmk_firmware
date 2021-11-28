316418e668ca2e95e3b424001fd0f1558d90ec8e
Ga68 <github.ga68.e@grxme.com>
2021-11-27 16:37:08 -0800

    Number-Symbol layer layout change

--- keyboards/sofle/keymaps/Ga68/keymap.c
+++ keyboards/sofle/keymaps/Ga68/keymap.c
@@ -106,9 +106,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMPADSYM] = LAYOUT(
         KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,               KC_F7  , KC_F8, KC_F9, KC_F10, KC_F11 , KC_F12 ,
-        KC_TILD, KC_GRV , KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,               KC_SLSH, KC_7 , KC_8 , KC_9  , KC_EQL , KC_PIPE,
-        KC_LBRC, KC_RBRC, KC_DLR , KC_PERC, KC_CIRC, KC_LCBR,               KC_ASTR, KC_4 , KC_5 , KC_6  , KC_MINS, KC_PLUS,
-        KC_UNDS, KC_BSLS, KC_EXLM, KC_AT  , KC_HASH, KC_RCBR, _____, _____, KC_0   , KC_1 , KC_2 , KC_3  , KC_DOT , KC_ENT ,
+        KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, KC_GRV , KC_TILD,               KC_SLSH, KC_7 , KC_8 , KC_9  , KC_EQL , KC_MINS,
+        KC_LT  , KC_GT  , KC_DLR , KC_PERC, KC_CIRC, KC_PIPE,               KC_ASTR, KC_4 , KC_5 , KC_6  , KC_MINS, KC_PLUS,
+        KC_LBRC, KC_RBRC, KC_EXLM, KC_AT  , KC_HASH, __x__  , _____, _____, __x__  , KC_1 , KC_2 , KC_3  , KC_DOT , KC_ENT ,
 
                        KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, TO(_NAV), OSM(MOD_RSFT), TO(_BASE), KC_RGUI, KC_RALT, KC_RCTL
         ),
