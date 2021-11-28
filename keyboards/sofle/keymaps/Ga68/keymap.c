f189e0b7b27a62c73111fcf8812aaa760395d050
Ga68 <github.ga68.e@grxme.com>
2021-11-27 16:36:53 -0800

    Numerous code and keycode legibility improvements - and a few tap-hold additions

--- keyboards/sofle/keymaps/Ga68/keymap.c
+++ keyboards/sofle/keymaps/Ga68/keymap.c
@@ -20,7 +20,8 @@ enum my_layers {
 // -----------------------
 //   CB = Combo
 //   TH = Tap Hold
-//   UKC = User Key Code
+//   UKC = User Keycode
+//   ZM = Zoom
 
 #define _____ KC_TRANSPARENT
 #define __x__ KC_NO
@@ -28,21 +29,27 @@ enum my_layers {
 enum my_keycodes {
     // Combos
     //   See process_combo_keycode_user to see these keycodes' implementation.
-    CB_PRNS = SAFE_RANGE, // ()
-    CB_BRCS, // []
-    CB_CBRS, // {}
-    CB_LTGT, // <>
+    CB_PARENS = SAFE_RANGE, // ()
+    CB_BRACES, // []
+    CB_CURLY_BRACES, // {}
+    CB_ANGLE_BRACES, // <>
+    // Other
+    UKC_QUESTION,
+        // Using this as a substitute for a KC_QUESTION tap-hold since the KC_QUESTION keycode
+        // overlaps with KC_SLASH, since KC_QUESTION = LSHFT(KC_SLASH)
 };
 
-#define UKC_CUT  LCMD(KC_X)
-#define UKC_COPY LCMD(KC_C)
-#define UKC_PSTE LCMD(KC_V)
-#define UKC_PPST LCMD(LALT(LSFT(KC_V))) // Plain Paste = paste a plain copy of the text
-#define UKC_UNDO LCMD(KC_Z)
-#define UKC_REDO LCMD(LSFT(KC_Z))
+#define UKC_CUT         LCMD(KC_X)
+#define UKC_COPY        LCMD(KC_C)
+#define UKC_PASTE       LCMD(KC_V)
+#define UKC_PSTE        UKC_PASTE
+#define UKC_PLAIN_PASTE LALT(LSFT(UKC_PASTE)) // Plain Paste = paste a plain copy of the text
+#define UKC_PPST        UKC_PLAIN_PASTE
+#define UKC_UNDO        LCMD(KC_Z)
+#define UKC_REDO        LCMD(LSFT(KC_Z))
 
-#define UKC_IQUS   LSFT(LALT(KC_SLASH)) // ¿
-#define UKC_EMDASH LSFT(LALT(KC_MINUS))
+#define UKC_INV_QUESTION LSFT(LALT(KC_SLASH))  // ¿
+#define UKC_IQUS         UKC_INV_QUESTION
 
 #define UKC_WD_LEFT RALT(KC_LEFT)
 #define UKC_WD_RGHT RALT(KC_RGHT)
@@ -54,9 +61,13 @@ enum my_keycodes {
 // See process_tap_hold_keycode_user to see these keycodes' implementation.
 #define TH_Z_ZOOM    LT(_BASE, KC_Z)
 #define TH_ESC_CAPS  LT(_BASE, KC_ESC)
-#define TH_COLN_SCLN LT(_BASE, KC_COLN)
-#define TH_MINS_UNDS LT(_BASE, KC_MINS)
-#define TH_QUES_IQUS LT(_BASE, KC_QUES)
+#define TH_COLN_SCLN LT(_BASE, KC_COLON)
+#define TH_MINS_UNDS LT(_BASE, KC_MINUS)
+#define TH_QUES_IQUS LT(_BASE, UKC_QUESTION)
+#define TH_QUOT_DQUO LT(_BASE, KC_QUOTE)
+#define TH_SLSH_BSLS LT(_BASE, KC_SLASH)
+#define TH_DOT_RPRN  LT(_BASE, KC_DOT)
+#define TH_COMM_LPRN LT(_BASE, KC_COMMA)
 
 #define TH_X_CUT  LT(_BASE, KC_X)
 #define TH_C_COPY LT(_BASE, KC_C)
@@ -67,6 +78,16 @@ enum my_keycodes {
 #define TH_UP_GUI    LT(_NAV, KC_UP)
 #define TH_DOWN_GUI  LT(_NAV, KC_DOWN)
 
+// Zoom
+#define ZM_TG_AUDO  LCMD(LSFT(KC_A))
+#define ZM_TG_VIDO  LCMD(LSFT(KC_V))
+#define ZM_TG_VIEW  LCMD(LSFT(KC_W))
+#define ZM_TG_CAM   LCMD(LSFT(KC_N))
+#define ZM_SCR_SHR  LCMD(LSFT(KC_S))
+#define ZM_CLS_MEET LCMD(KC_W)
+#define ZM_WDW_FULL LCMD(LCTL(KC_F))
+#define ZM_WDW_HALF MEH(KC_6)
+
 
 // --------------
 // --- Keymap ---
@@ -75,10 +96,10 @@ enum my_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_COLEMAK] = LAYOUT(
-        TH_ESC_CAPS , KC_1     , KC_2    , KC_3     , KC_4     , KC_5,                   KC_6, KC_7, KC_8   , KC_9  , KC_0        , __x__       ,
-        TH_QUES_IQUS, KC_Q     , KC_W    , KC_F     , KC_P     , KC_G,                   KC_J, KC_L, KC_U   , KC_Y  , TH_COLN_SCLN, TH_MINS_UNDS,
-        KC_TAB      , KC_A     , KC_R    , KC_S     , KC_T     , KC_D,                   KC_H, KC_N, KC_E   , KC_I  , KC_O        , KC_QUOT     ,
-        TO(_NAV)    , TH_Z_ZOOM, TH_X_CUT, TH_C_COPY, TH_V_PSTE, KC_B, KC_MUTE, KC_MUTE, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH     , KC_ENT      ,
+        TH_ESC_CAPS , KC_1     , KC_2    , KC_3     , KC_4     , KC_5,                   KC_6, KC_7, KC_8        , KC_9       , KC_0        , __x__       ,
+        TH_QUES_IQUS, KC_Q     , KC_W    , KC_F     , KC_P     , KC_G,                   KC_J, KC_L, KC_U        , KC_Y       , TH_COLN_SCLN, TH_MINS_UNDS,
+        KC_TAB      , KC_A     , KC_R    , KC_S     , KC_T     , KC_D,                   KC_H, KC_N, KC_E        , KC_I       , KC_O        , TH_QUOT_DQUO,
+        TO(_NAV)    , TH_Z_ZOOM, TH_X_CUT, TH_C_COPY, TH_V_PSTE, KC_B, KC_MUTE, KC_MUTE, KC_K, KC_M, TH_COMM_LPRN, TH_DOT_RPRN, TH_SLSH_BSLS, KC_ENT      ,
         
                                  KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, TO(_NMSY), OSM(MOD_RSFT), KC_SPC, KC_RGUI, KC_RALT, KC_RCTL
     ),
@@ -102,82 +123,89 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 
     [_ZOOM] = LAYOUT(
-        __x__, __x__, __x__, __x__, __x__, __x__,               __x__     , __x__ , __x__, __x__, __x__, __x__,
-        __x__, __x__, __x__, __x__, __x__, __x__,               __x__     , __x__ , __x__, __x__, __x__, __x__,
-        __x__, __x__, __x__, __x__, __x__, __x__,               __x__     , __x__ , __x__, __x__, __x__, __x__,
-        __x__, __x__, __x__, __x__, __x__, __x__, _____, _____, SGUI(KC_V), __x__ , __x__, __x__, __x__, __x__,
+        __x__, __x__, __x__, __x__, __x__, __x__,               __x__     , __x__ , __x__, __x__      , __x__      , __x__      ,
+        __x__, __x__, __x__, __x__, __x__, __x__,               __x__     , __x__ , __x__, __x__      , __x__      , __x__      ,
+        __x__, __x__, __x__, __x__, __x__, __x__,               ZM_TG_CAM , __x__ , __x__, __x__      , __x__      , ZM_CLS_MEET,
+        __x__, __x__, __x__, __x__, __x__, __x__, _____, _____, ZM_TG_VIEW, __x__ , __x__, ZM_WDW_FULL, ZM_WDW_HALF, KC_ENT     ,
 
-                __x__, __x__, __x__, KC_SPC, SGUI(KC_V), SGUI(KC_A), TO(_BASE), KC_SPC, SGUI(KC_S), SGUI(KC_W)
+                __x__, __x__, __x__, KC_SPC, ZM_TG_VIDO, ZM_TG_AUDO, TO(_BASE), ZM_TG_VIDO, ZM_SCR_SHR, KC_SPC
         ),
 };
 
 // --------------
 // --- Combos ---
 // --------------
-const uint16_t PROGMEM combo_parentheses[] = {KC_DOT, KC_COMM, COMBO_END};
-const uint16_t PROGMEM combo_braces[] = {KC_LBRC, KC_RBRC, COMBO_END};
-const uint16_t PROGMEM combo_curly_braces[] = {KC_LCBR, KC_RCBR, COMBO_END};
-const uint16_t PROGMEM combo_less_than_greater_than[] = {KC_LT, KC_GT, COMBO_END};
-const uint16_t PROGMEM combo_paste_plain[] = {KC_C, KC_V, COMBO_END};
+#ifdef COMBO_ENABLE
 
-uint16_t COMBO_LEN = 5;
-combo_t key_combos[] = {
-    COMBO(combo_parentheses, CB_PRNS),
-    COMBO(combo_braces, CB_BRCS),
-    COMBO(combo_curly_braces, CB_CBRS),
-    COMBO(combo_less_than_greater_than, CB_LTGT),
-    COMBO(combo_paste_plain, UKC_PPST),
-};
+    const uint16_t PROGMEM combo_parentheses[] = {KC_DOT, KC_COMMA, COMBO_END};
+    const uint16_t PROGMEM combo_braces[] = {KC_LEFT_BRACE, KC_RIGHT_BRACE, COMBO_END};
+    const uint16_t PROGMEM combo_curly_braces[] = {KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, COMBO_END};
+    const uint16_t PROGMEM combo_less_than_greater_than[] = {KC_LT, KC_GT, COMBO_END};
+    const uint16_t PROGMEM combo_paste_plain[] = {KC_C, KC_V, COMBO_END};
 
-bool process_combo_keycode_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CB_PRNS:
-            if (record->event.pressed) {
-                tap_code16(KC_LPRN);
-                tap_code16(KC_RPRN);
-                tap_code(KC_LEFT);
-            }
-            return false;
-        case CB_BRCS:
-            if (record->event.pressed) {
-                tap_code16(KC_LBRC);
-                tap_code16(KC_RBRC);
-                tap_code(KC_LEFT);
-            }
-            return false;
-        case CB_CBRS:
-            if (record->event.pressed) {
-                tap_code16(KC_LCBR);
-                tap_code16(KC_RCBR);
-                tap_code(KC_LEFT);
-            }
-            return false;
-        case CB_LTGT:
-            if (record->event.pressed) {
-                tap_code16(KC_LT);
-                tap_code16(KC_GT);
-                tap_code(KC_LEFT);
-            }
-            return false;
+    uint16_t COMBO_LEN = 5;
+    combo_t key_combos[] = {
+        COMBO(combo_parentheses, CB_PARENS),
+        COMBO(combo_braces, CB_BRACES),
+        COMBO(combo_curly_braces, CB_CURLY_BRACES),
+        COMBO(combo_less_than_greater_than, CB_ANGLE_BRACES),
+        COMBO(combo_paste_plain, UKC_PLAIN_PASTE),
+    };
+
+    bool process_combo_keycode_user(uint16_t keycode, keyrecord_t *record) {
+        switch (keycode) {
+            case CB_PARENS:
+                if (record->event.pressed) {
+                    tap_code16(KC_LEFT_PAREN);
+                    tap_code16(KC_RIGHT_PAREN);
+                    tap_code(KC_LEFT);
+                }
+                return false;
+            case CB_BRACES:
+                if (record->event.pressed) {
+                    tap_code16(KC_LEFT_BRACE);
+                    tap_code16(KC_RIGHT_BRACE);
+                    tap_code(KC_LEFT);
+                }
+                return false;
+            case CB_CURLY_BRACES:
+                if (record->event.pressed) {
+                    tap_code16(KC_LEFT_CURLY_BRACE);
+                    tap_code16(KC_RIGHT_CURLY_BRACE);
+                    tap_code(KC_LEFT);
+                }
+                return false;
+            case CB_ANGLE_BRACES:
+                if (record->event.pressed) {
+                    tap_code16(KC_LEFT_ANGLE_BRACE);
+                    tap_code16(KC_RIGHT_ANGLE_BRACE);
+                    tap_code(KC_LEFT);
+                }
+                return false;
+        }
+        return true;
     }
-    return true;
-}
 
+#endif
 
 // -----------------
 // --- Overrides ---
 // -----------------
 
-const key_override_t delete_key_override_shift = ko_make_basic(MOD_MASK_SHIFT, KC_BSPACE, KC_DELETE);
-const key_override_t delete_key_override_ctrl = ko_make_basic(MOD_MASK_CTRL, KC_BSPACE, KC_DELETE);
+#ifdef KEY_OVERRIDE_ENABLE
 
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override_shift,
-    &delete_key_override_ctrl,
-    NULL // Terminate the array of overrides
-};
+    // Shift Backspace = Delete
+    const key_override_t delete_key_override_shift = ko_make_basic(MOD_MASK_SHIFT, KC_BACKSPACE, KC_DELETE);
+    // Ctrl Backspace = Delete
+    const key_override_t delete_key_override_ctrl = ko_make_basic(MOD_MASK_CTRL, KC_BACKSPACE, KC_DELETE);
 
+    const key_override_t **key_overrides = (const key_override_t *[]){
+        &delete_key_override_shift,
+        &delete_key_override_ctrl,
+        NULL // Terminate the array of overrides
+    };
+
+#endif
 
 // ---------------
 // --- Tapping ---
@@ -205,23 +233,51 @@ bool process_tap_hold_keycode_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case TH_COLN_SCLN:
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_COLN);
+                tap_code16(KC_COLON);
             } else if (record->event.pressed) {
-                tap_code(KC_SCLN);
+                tap_code(KC_SEMICOLON);
             }
             return false;
         case TH_MINS_UNDS:
             if (record->tap.count && record->event.pressed) {
-                tap_code(KC_MINS);
+                tap_code(KC_MINUS);
             } else if (record->event.pressed) {
-                tap_code16(KC_UNDS);
+                tap_code16(KC_UNDERSCORE);
             }
             return false;
         case TH_QUES_IQUS:
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_QUES);
+                tap_code16(KC_QUESTION);
             } else if (record->event.pressed) {
-                tap_code16(UKC_IQUS);
+                tap_code16(UKC_INV_QUESTION);
+            }
+            return false;
+        case TH_QUOT_DQUO:
+            if (record->tap.count && record->event.pressed) {
+                tap_code(KC_QUOTE);
+            } else if (record->event.pressed) {
+                tap_code16(KC_DOUBLE_QUOTE);
+            }
+            return false;
+        case TH_SLSH_BSLS:
+            if (record->tap.count && record->event.pressed) {
+                tap_code(KC_SLASH);
+            } else if (record->event.pressed) {
+                tap_code16(KC_BACKSLASH);
+            }
+            return false;
+        case TH_DOT_RPRN:
+            if (record->tap.count && record->event.pressed) {
+                tap_code(KC_DOT);
+            } else if (record->event.pressed) {
+                tap_code16(KC_RIGHT_PAREN);
+            }
+            return false;
+        case TH_COMM_LPRN:
+            if (record->tap.count && record->event.pressed) {
+                tap_code(KC_COMMA);
+            } else if (record->event.pressed) {
+                tap_code16(KC_LEFT_PAREN);
             }
             return false;
         case TH_LEFT_GUI:
@@ -233,9 +289,9 @@ bool process_tap_hold_keycode_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case TH_RGHT_GUI:
             if (record->tap.count && record->event.pressed) {
-                tap_code(KC_RGHT);
+                tap_code(KC_RIGHT);
             } else if (record->event.pressed) {
-                tap_code16(LGUI(KC_RGHT));
+                tap_code16(LGUI(KC_RIGHT));
             }
             return false;
         case TH_UP_GUI:
@@ -270,7 +326,7 @@ bool process_tap_hold_keycode_user(uint16_t keycode, keyrecord_t *record) {
             if (record->tap.count && record->event.pressed) {
                 tap_code(KC_V);
             } else if (record->event.pressed) {
-                tap_code16(UKC_PSTE);
+                tap_code16(UKC_PASTE);
             }
             return false;
     }
@@ -300,9 +356,9 @@ bool process_tap_hold_keycode_user(uint16_t keycode, keyrecord_t *record) {
         if (index == 0 || index == 1) {
             // For now I'm doing the same thing for both encoders
             if (clockwise) {
-                tap_code(KC_VOLU);
+                tap_code(KC_AUDIO_VOL_UP);
             } else {
-                tap_code(KC_VOLD);
+                tap_code(KC_AUDIO_VOL_DOWN);
             }
         }
         return true;
--- keyboards/sofle/keymaps/Ga68/keymap.h
+++ keyboards/sofle/keymaps/Ga68/keymap.h
@@ -3,3 +3,25 @@
 #pragma once
 
 #include QMK_KEYBOARD_H
+
+// -----------------
+// --- Verbosity ---
+// -----------------
+// Some more verbose keycode names (aliases), simply as a personal preference for legibility when
+// space isn't the primary concern.
+
+#define KC_BACKSPACE           KC_BSPACE
+#define KC_SEMICOLON           KC_SCOLON
+#define KC_BACKSLASH           KC_BSLASH
+#define KC_PERIOD              KC_DOT
+
+// Allow for "brace" and "bracket" to be used interchangeably
+//   Braces [ ], Curly Braces { }, Angled Braces < >
+#define KC_RIGHT_BRACKET       KC_RBRACKET
+#define KC_RIGHT_BRACE         KC_RBRACKET
+#define KC_LEFT_BRACKET        KC_LBRACKET
+#define KC_LEFT_BRACE          KC_LBRACKET
+#define KC_RIGHT_CURLY_BRACKET KC_RIGHT_CURLY_BRACE
+#define KC_LEFT_CURLY_BRACKET  KC_LEFT_CURLY_BRACE
+#define KC_RIGHT_ANGLE_BRACE   KC_RIGHT_ANGLE_BRACKET
+#define KC_LEFT_ANGLE_BRACE    KC_LEFT_ANGLE_BRACKET
