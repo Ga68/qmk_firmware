92122b271134bb676f21829ec2f133e2f17164c1
Ga68 <github.ga68.e@grxme.com>
2022-01-04 08:54:31 -0800

    made caps_word toggle-able using the caps_word key

--- keyboards/sofle/keymaps/Ga68/caps_word.h
+++ keyboards/sofle/keymaps/Ga68/caps_word.h
@@ -3,5 +3,7 @@
 #include QMK_KEYBOARD_H
 
 bool process_caps_word(uint16_t keycode, keyrecord_t* record);
+void toggle_caps_word_status(void);
 
 bool g_caps_word_enabled = false;
+bool g_caps_word_shifted = false;
