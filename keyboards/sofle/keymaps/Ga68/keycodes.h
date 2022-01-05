8dfd7859323d06a4f6da0798258a17f5a314669a
Ga68 <github.ga68.e@grxme.com>
2022-01-04 23:35:42 -0800

    minor refactor

--- keyboards/sofle/keymaps/Ga68/keycodes.h
+++ keyboards/sofle/keymaps/Ga68/keycodes.h
@@ -1,5 +1,18 @@
 #pragma once
 
+// ----------------
+// --- Mod Taps ---
+// ----------------
+
+#define MT_LG(k) MT(MOD_LGUI, k)
+#define MT_LA(k) MT(MOD_LALT, k)
+#define MT_LC(k) MT(MOD_LCTL, k)
+#define MT_LS(k) MT(MOD_LSFT, k)
+#define MT_RG(k) MT(MOD_RGUI, k)
+#define MT_RA(k) MT(MOD_RALT, k)
+#define MT_RC(k) MT(MOD_RCTL, k)
+#define MT_RS(k) MT(MOD_RSFT, k)
+
 // ----------------------
 // --- Keycode Legend ---
 // ----------------------
--- keyboards/sofle/keymaps/Ga68/keymap.c
+++ keyboards/sofle/keymaps/Ga68/keymap.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "keycodes.h"
 
 // --------------
 // --- Layers ---
@@ -15,19 +14,11 @@ enum my_layers {
     _CAPS_WORD,
 };
 
-#define MT_LG(k) MT(MOD_LGUI, k)
-#define MT_LA(k) MT(MOD_LALT, k)
-#define MT_LC(k) MT(MOD_LCTL, k)
-#define MT_LS(k) MT(MOD_LSFT, k)
-#define MT_RG(k) MT(MOD_RGUI, k)
-#define MT_RA(k) MT(MOD_RALT, k)
-#define MT_RC(k) MT(MOD_RCTL, k)
-#define MT_RS(k) MT(MOD_RSFT, k)
-
 // ----------------
 // --- Features ---
 // ----------------
 
+#include "keycodes.h"
 #include "tapping.c"
 #include "caps_word.c"
 
