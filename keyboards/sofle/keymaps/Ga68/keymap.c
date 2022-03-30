fed67e3092947bb2cc0dfeee8351fd0729248346
Ga68 <github.ga68.e@grxme.com>
2022-03-29 23:12:33 -0700

    updating mouse layer

--- keyboards/sofle/keymaps/Ga68/keymap.c
+++ keyboards/sofle/keymaps/Ga68/keymap.c
@@ -48,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 
     [_MOUSE] = LAYOUT(
-        __x__   , __x__   , __x__  , __x__   , __x__   , __x__   ,               __x__, __x__  , __x__  , __x__  , __x__  , __x__       ,
-        KC_WH_R , KC_WH_D , KC_WH_L, KC_MS_U , KC_BTN1 , KC_BTN2 ,               __x__, __x__  , __x__  , __x__  , __x__  , __x__       ,
-        KC_ENTER, KC_WH_U , KC_MS_L, KC_MS_D , KC_MS_R , __x__   ,               __x__, KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL, KC_BACKSPACE,
-        KC_SPACE, UKC_UNDO, UKC_CUT, UKC_COPY, UKC_PSTE, UKC_REDO, __x__, __x__, __x__, __x__  , __x__  , __x__  , __x__  , KC_DELETE   ,
+        __x__ , __x__    , __x__  , __x__   , __x__   , __x__   ,               __x__, __x__  , __x__  , __x__  , __x__  , __x__       ,
+        __x__ , __x__    , KC_BTN1, KC_MS_U , KC_BTN2 , __x__   ,               __x__, __x__  , __x__  , __x__  , __x__  , __x__       ,
+        __x__ , MEH(KC_Q), KC_MS_L, KC_MS_D , KC_MS_R , __x__   ,               __x__, KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL, KC_BACKSPACE,
+        __x__ , UKC_UNDO , UKC_CUT, UKC_COPY, UKC_PSTE, UKC_REDO, __x__, __x__, __x__, __x__  , __x__  , __x__  , __x__  , KC_DELETE   ,
 
                                           __x__, __x__, __x__, __x__, __o__, __x__, KC_BTN1, KC_BTN2, __x__, __x__
         ),
