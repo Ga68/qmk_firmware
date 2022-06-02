0edbc8e9f30b5fac7ca8720406e058bcc1c1c1a3
Ga68 <github.ga68.e@grxme.com>
2022-06-02 07:43:51 -0700

    Reverting DHM mod 078a002ff886eecc61450bc374a8b3907a0bfb72

--- keyboards/sofle/keymaps/Ga68/keymap.c
+++ keyboards/sofle/keymaps/Ga68/keymap.c
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         __x__        , KC_1       , KC_2       , KC_3       , KC_4       , KC_5,                   KC_6, KC_7       , KC_8       , KC_9       , KC_0       , __x__         ,
-        __x__        , KC_Q       , KC_W       , KC_F       , KC_P       , KC_B,                   KC_J, KC_L       , KC_U       , KC_Y       , KC_COLON   , KC_EQL        ,
-        KC_MINS      , MT_LC(KC_A), MT_LA(KC_R), MT_LS(KC_S), MT_LG(KC_T), KC_G,                   KC_M, MT_RG(KC_N), MT_RS(KC_E), MT_RA(KC_I), MT_RC(KC_O), KC_QUOT       ,
-        KC_LEFT_PAREN, KC_Z       , KC_X       , KC_C       , KC_D       , KC_V, KC_MUTE, KC_MUTE, KC_K, KC_H       , KC_COMMA   , KC_DOT     , KC_SLASH   , KC_RIGHT_PAREN,
+        __x__        , KC_Q       , KC_W       , KC_F       , KC_P       , KC_G,                   KC_J, KC_L       , KC_U       , KC_Y       , KC_COLON   , KC_EQL        ,
+        KC_MINS      , MT_LC(KC_A), MT_LA(KC_R), MT_LS(KC_S), MT_LG(KC_T), KC_D,                   KC_H, MT_RG(KC_N), MT_RS(KC_E), MT_RA(KC_I), MT_RC(KC_O), KC_QUOT       ,
+        KC_LEFT_PAREN, KC_Z       , KC_X       , KC_C       , KC_V       , KC_B, KC_MUTE, KC_MUTE, KC_K, KC_M       , KC_COMMA   , KC_DOT     , KC_SLASH   , KC_RIGHT_PAREN,
         
               __x__, __x__, MEH_T(KC_DELETE), LT(_NAV, KC_BACKSPACE), LT(_MOUSE, KC_TAB), KC_ENTER, LT(_LOWER, KC_SPACE), MEH_T(UKC_APP_SWITCH), __x__, __x__
     ),
