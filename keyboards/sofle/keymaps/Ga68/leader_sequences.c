0e807f0b4a16fdb769739cc51a325244906f18bf
Ga68 <github.ga68.e@grxme.com>
2022-06-15 10:59:11 -0700

    new leader sequences

--- keyboards/sofle/keymaps/Ga68/leader_sequences.c
+++ keyboards/sofle/keymaps/Ga68/leader_sequences.c
@@ -1,18 +1,18 @@
 #include "leader.h"
 
+
 void *leader_zoom_func(uint16_t keycode) {
     switch (keycode) {
         case KC_W: // Leave the Zoom meeting
             tap_code16(MEH(KC_Z));  // Switch to Zoom
-            DELAY;
+            DELAY_SHORT;
             tap_code16(UKC_MENU);   // Move to the menu
             SEND_STRING("win");     // Open the Window menu and move the Zoom Meeting window
             DELAY_SHORT;
             tap_code16(KC_DOWN);
             tap_code16(KC_UP);
-            DELAY_SHORT;
             tap_code16(KC_ENTER);
-            DELAY_LONG;                      
+            DELAY;                      
             tap_code16(LCMD(KC_W)); // Close the window
             return NULL;
     }
@@ -24,6 +24,30 @@ void *leader_outlook_func(uint16_t keycode) {
         case KC_U: // Mark message as unread
             tap_code16(LCMD(LSFT(KC_T)));
             return NULL;
+        case KC_M:
+            tap_code16(MEH(KC_O));  // Open Outlook
+            tap_code16(LCMD(KC_1)); // Mail pane
+            return NULL;
+        case KC_C:
+            tap_code16(MEH(KC_O));  // Open Outlook
+            tap_code16(LCMD(KC_2)); // Calendarpane
+            return NULL;
+        case KC_R:
+            tap_code16(MEH(KC_O));  // Open Outlook
+            tap_code16(LCMD(KC_1)); // Mail pane
+            tap_code16(LCMD(KC_G)); // Go to folder
+            SEND_STRING("calendar requests\n");
+            return NULL;        
+    }
+    return NULL;
+}
+
+void *leader_slack_func(uint16_t keycode) {
+    switch (keycode) {
+        case KC_T: // Jump to Slack conversation (reference to the cmd-T keystroke)
+            tap_code16(MEH(KC_S));  // Open Slack
+            tap_code16(LCMD(KC_T)); // Jump to...
+            return NULL;
     }
     return NULL;
 }
@@ -34,6 +58,8 @@ void *leader_start_func(uint16_t keycode) {
             return leader_outlook_func;
         case KC_Z:
             return leader_zoom_func;
+        case KC_S:
+            return leader_slack_func;
     }
     return NULL;
 }
